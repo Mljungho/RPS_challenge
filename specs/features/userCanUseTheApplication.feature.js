04caaaee2ea3327a7302b4c265f33b5c5478c78a
Mljungho <mathias.ljungholm@gmail.com>
2021-12-06 23:12:22 +0100

    cypress callback fails

--- specs/features/userCanUseTheApplication.feature.js
+++ specs/features/userCanUseTheApplication.feature.js
@@ -1,31 +1,32 @@
-desribe("User can press buttons and play rock paper scissiors", () => {
+describe("User can press buttons and play rock paper scissiors", () => {
   beforeEach(() => {
     cy.visit("http://localhost:3000");
+    
+    describe("Three buttons"), () => {
+      it("display Rock button", () => {
+        cy.get("button[id=Rock]").should("contain", "Rock");
+      });
+  
+      it("display Paper button", () => {
+        cy.get("button[id=Paper]").should("contain", "Paper");
+      });
+  
+      it("display Scissor button", () => {
+        cy.get("button[id=Scissor]").should("contain", "Scissor");
+      });
+    }
   });
 
-  describe("Three buttons"), () => {
-    it("display Rock button", () => {
-      cy.get("#rock-button").should("contain", "Rock");
-    });
 
-    it("display Paper button", () => {
-      cy.get("#paper-button").should("contain", "Paper");
-    });
+  describe("player chose Rock", () => {
 
-    it("display Scissor button", () => {
-      cy.get("#scissor-button").should("contain", "Scissor");
-    });
-
-
-  }
-  
-  desribe("chose Rock", () => {
     it("display player choice in span element", () => {
+      cy.get("button[id=Rock]").click()
       cy.div().should("contain", "Rock");
     });
   });
 
-  desribe("computer choice", () => {
+  describe("computer choice", () => {
     it("display computer choice", () => {
       cy.get("#computer-choice").should("exist");
     });
