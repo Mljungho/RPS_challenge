bddc768cb1f2e02a98149a958ac5e080d08cb3b6
Mljungho <mathias.ljungholm@gmail.com>
2021-12-07 21:10:48 +0100

    set up rudimentary test spec. Can find buttons and will have to supply functions to them.

--- specs/features/userCanUseTheApplication.feature.js
+++ specs/features/userCanUseTheApplication.feature.js
@@ -1,22 +1,28 @@
 describe("User can press buttons and play rock paper scissiors", () => {
-  beforeEach(() => {
+  before(() => {
     cy.visit("http://localhost:3000");
-    
-    describe("Three buttons"), () => {
-      it("display Rock button", () => {
-        cy.get("button[id=Rock]").should("contain", "Rock");
-      });
-  
-      it("display Paper button", () => {
-        cy.get("button[id=Paper]").should("contain", "Paper");
-      });
-  
-      it("display Scissor button", () => {
-        cy.get("button[id=Scissor]").should("contain", "Scissor");
-      });
-    }
   });
 
+  describe("user can see UI elements", () => {
+    it("is expected to see Hello World",() => {
+      cy.get("h1").should("contain.text", "Hello world")
+    })
+  })
+  
+  describe("Three buttons", () => {
+    it("display Rock button", () => {
+      cy.get('button[id=rock]').should("contain", "Rock");
+    });
+
+    it("display Paper button", () => {
+      cy.get("button[id=paper]").should("contain", "Paper");
+    });
+
+    it("display Scissor button", () => {
+      cy.get("button[id=scissor]").should("contain", "Scissor");
+    });
+  })
+  
 
   describe("player chose Rock", () => {
 
